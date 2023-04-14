ec53a9ddd11a71589aa8f42211e0964a59c192b3
lavrentious <lavrent0@yandex.ru>
2023-04-14 20:11:16 +0700

    refactor: move edit profile form outside of modal

---
+++ src/modules/users/components/Profile/EditProfileForm.tsx
@@ -0,0 +1,158 @@
+import { useFormik } from "formik";
+import React from "react";
+import { Button, Form, FormControl } from "react-bootstrap";
+import {
+  CheckLg as SubmitIcon,
+  XLg as CancelIcon,
+} from "react-bootstrap-icons";
+import { toast } from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { ApiError } from "src/modules/common/api";
+import LoadingButton from "src/modules/common/components/LoadingButton/LoadingButton";
+import { validators } from "src/modules/users/utils/validations";
+import { RootState, useAppDispatch } from "src/store";
+import { setUser as setLoggedUser } from "src/store/auth.reducer";
+import * as yup from "yup";
+import { User } from "../../models/user.model";
+import { UserService } from "../../services/user.service";
+
+interface Values {
+  email?: string;
+  username?: string;
+  name?: string;
+}
+interface IFieldProps {
+  f: ReturnType<typeof useFormik<Values>>;
+  field: keyof Values;
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+}
+const Field: React.FC<IFieldProps> = ({
+  f,
+  field,
+  label,
+  placeholder,
+  required,
+}) => {
+  return (
+    <Form.Group className="my-2">
+      {label && (
+        <Form.Label htmlFor={field}>
+          {label}
+          {required && <span className="text-danger"> *</span>}
+        </Form.Label>
+      )}
+      <FormControl
+        id={field}
+        onBlur={f.handleBlur}
+        value={f.values[field]}
+        onChange={f.handleChange}
+        placeholder={placeholder ?? ""}
+        isInvalid={f.touched[field] && !!f.errors[field]}
+        isValid={f.touched[field] && !f.errors[field]}
+      />
+    </Form.Group>
+  );
+};
+
+const validationSchema = yup.object().shape({
+  username: validators.username,
+  email: validators.email,
+  name: validators.name,
+});
+
+interface EditProfileFormProps {
+  user: User;
+  setUser?: (u: User) => void;
+  setVisible: (v: boolean) => void;
+}
+
+const EditProfileForm: React.FC<EditProfileFormProps> = ({
+  user,
+  setUser,
+  setVisible,
+}) => {
+  const loggedUser = useSelector((state: RootState) => state.auth.user);
+  const dispatch = useAppDispatch();
+
+  const f = useFormik<Values>({
+    initialValues: {
+      email: user.email,
+      username: user.username || "",
+      name: user.name || "",
+    },
+    onSubmit: async ({ email, name, username }) => {
+      const values = {
+        email,
+        name: name || null,
+        username: username || null,
+      };
+      await UserService.update(user._id, values, user)
+        .then((newUser) => {
+          if (user._id === loggedUser?.id) {
+            dispatch(
+              setLoggedUser({
+                id: user._id,
+                role: newUser.role,
+                name: newUser.name ?? undefined,
+                username: newUser.username ?? undefined,
+              })
+            );
+          }
+          toast.success("Профиль изменён");
+          setVisible(false);
+          if (setUser) setUser(new User(newUser));
+        })
+        .catch((e: ApiError) => {
+          const msg = e.response?.data.message;
+          toast.error(msg ?? e.message);
+        });
+    },
+    validationSchema,
+    validateOnBlur: true,
+  });
+  return (
+    <>
+      <Form onSubmit={f.handleSubmit} id="editProfile">
+        <Field
+          f={f}
+          field="email"
+          label="Email"
+          placeholder={user.email}
+          required
+        />
+        <Field
+          f={f}
+          field="username"
+          label="Логин"
+          placeholder={user.username || "Введите логин"}
+        />
+        <Field
+          f={f}
+          field="name"
+          label="Имя"
+          placeholder={user.name || "Введите имя"}
+        />
+      </Form>
+      <Button
+        className="me-2"
+        variant="secondary"
+        onClick={() => setVisible(false)}
+      >
+        <CancelIcon /> Отмена
+      </Button>
+      <LoadingButton
+        variant="success"
+        form="editProfile"
+        type="submit"
+        isLoading={f.isSubmitting}
+        icon={<SubmitIcon />}
+      >
+        Подтвердить
+      </LoadingButton>
+    </>
+  );
+};
+
+export default EditProfileForm;
