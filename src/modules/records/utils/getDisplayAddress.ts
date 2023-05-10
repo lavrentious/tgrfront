1461741e5a12e86e355bfc433112fee627431175
lavrentious <lavrent0@yandex.ru>
2023-05-10 17:10:51 +0700

    style: reorder display name street, house, city, region

--- src/modules/records/utils/getDisplayAddress.ts
+++ src/modules/records/utils/getDisplayAddress.ts
@@ -2,7 +2,7 @@ import { Address } from "../models/record.model";
 
 export function getDisplayAddress(address: Address): string {
   const { region, city, street, house } = address;
-  return [region, city, street, house]
+  return [street, house, city, region]
     .filter((e) => e && e.length > 0)
     .join(", ");
 }
