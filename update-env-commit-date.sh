db76d3bba66c857f3253afbdd1bfe31e319914f9
lavrentious <lavrent0@yandex.ru>
2025-05-21 00:27:46 +0300

    fix: commit date env property

--- update-env-commit-date.sh
+++ update-env-commit-date.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 file_path=$1
-commit_date="REACT_APP_LAST_COMMIT_DATE=$(git log -1 --format=%aI --date=iso-strict)"
+commit_date="VITE_LAST_COMMIT_DATE=$(git log -1 --format=%aI --date=iso-strict)"
 
 if [ ! -f $file_path ]; then
   touch $file_path
 fi
 
-grep -v -E '^REACT_APP_LAST_COMMIT_DATE.+$' $file_path > /tmp/$file_path
+grep -v -E '^VITE_LAST_COMMIT_DATE.+$' $file_path > /tmp/$file_path
 cat /tmp/$file_path > $file_path
 rm /tmp/$file_path
 if [ -n "$(tail -c 1 $file_path)" ]; then
